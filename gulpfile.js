ebe906261d6d91b3d5e76ae63e299bc1fb6b3660
Kais Rekouche <kaisrec@hotmail.fr>
2017-04-20 10:32:31 +0100

    20170420

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver'),
+    typescript = require('gulp-typescript'),
+    tscConfig = require('./tsconfig.json');
+
+var appSrc = 'builds/development/',
+    tsSrc = 'process/typescript/';
+
+gulp.task('copylibs', function() {
+  return gulp
+    .src([
+      'node_modules/es6-shim/es6-shim.min.js',
+      'node_modules/systemjs/dist/system-polyfills.js',
+      'node_modules/angular2/bundles/angular2-polyfills.js',
+      'node_modules/systemjs/dist/system.src.js',
+      'node_modules/rxjs/bundles/Rx.js',
+      'node_modules/angular2/bundles/angular2.dev.js'
+    ])
+    .pipe(gulp.dest(appSrc + 'js/lib/angular2'));
+});
+
+gulp.task('typescript', function () {
+  return gulp
+    .src(tsSrc + '**/*.ts')
+    .pipe(typescript(tscConfig.compilerOptions))
+    .pipe(gulp.dest(appSrc + 'js/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(tsSrc + '**/*.ts', ['typescript']);
+});
+
+gulp.task('webserver', function() {
+  gulp.src(appSrc)
+    .pipe(webserver({
+      livereload: true,
+      port:'3000', // Open on http://localhost:3000
+      open: true
+    }));
+});
+
+gulp.task('default', ['copylibs', 'typescript', 'watch', 'webserver']);
---
+++ process/typescript/boot.es6.ts
@@ -0,0 +1,5 @@
+function add(x: number, y: number): number {
+    return x + y;
+}
+console.log(`hello Ecma6`);
+console.log(add(15,20));
